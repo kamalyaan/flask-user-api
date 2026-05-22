c4c5ad9b505336ae685d009125cdb5dda1a5bb8a
kamalyaan <144989113+kamalyaan@users.noreply.github.com>
2026-05-22 16:50:13 +0530

    Add Flask User CRUD API

---
+++ app.py
@@ -0,0 +1,37 @@
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "email": self.email}
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(name=data['name'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created", "user": new_user.to_dict()}), 201
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+if __name__ == '__main__':
+    app.run(debug=True)
